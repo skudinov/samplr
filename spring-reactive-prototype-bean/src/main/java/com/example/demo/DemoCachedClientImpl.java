8c422acbb336079e7af839d00c7ba4dfa0b40af8
Viachaslau_Kudzinau <viachaslau_kudzinau@epam.com>
2021-05-26 09:47:05 +0200

    Add using of Caffeine with Mono to cache result of DemoClient.getSomeValue

--- spring-reactive-prototype-bean/src/main/java/com/example/demo/DemoCachedClientImpl.java
+++ spring-reactive-prototype-bean/src/main/java/com/example/demo/DemoCachedClientImpl.java
@@ -2,19 +2,19 @@ package com.example.demo;
 
 import com.github.benmanes.caffeine.cache.Cache;
 import lombok.RequiredArgsConstructor;
-import reactor.cache.CacheMono;
 import reactor.core.publisher.Mono;
-import reactor.core.publisher.Signal;
 
 @RequiredArgsConstructor
 public class DemoCachedClientImpl implements DemoClient {
 
     private final DemoClient demoClient;
-    private final Cache<String, Signal<? extends String>> cache;
+    private final Cache<String, String> cache;
 
     @Override
     public Mono<String> getSomeValue(String param) {
-        return CacheMono.lookup(cache.asMap(), param)
-                .onCacheMissResume(() -> demoClient.getSomeValue(param));
+        return Mono.justOrEmpty(cache.getIfPresent(param))
+                .switchIfEmpty(demoClient
+                        .getSomeValue(param)
+                        .doOnNext(value -> cache.put(param, value)));
     }
 }
